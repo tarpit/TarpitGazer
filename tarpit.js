945a882f670f5235df60fcde0a2b91da045bb9b2
Eric Holk <eric.holk@gmail.com>
2013-02-22 22:58:56 -0500

    Some real visualization. The color mapping clearly needs some more help...

--- tarpit.js
+++ tarpit.js
@@ -82,6 +82,11 @@ function encode_jot(s) {
 }
 
 function decode_jot(x) {
+    // This is cheating, but it makes us more robust to errors.
+    if(x <= 0 || x >= 1) {
+        return '';
+    }
+
     var s = '';
     var y = 0.5;
     var z = 0.25;
@@ -102,4 +107,149 @@ function decode_jot(x) {
     //console.info(y);
 
     return s;
+}
+
+function K_cpst(x, k) {
+    return function () {
+        return k(function (y, k) {
+            return function () {
+                return k(x);
+            }
+        });
+    }
+}
+
+function S_cpst(x, k) {
+    return function() {
+        return k(function (y, k) {
+            return function () {
+                return k(function (z, k) {
+                    return function () {
+                        return x(z, function (xx) {
+                            return function () {
+                                return y(z, function (yy) {
+                                    return function () { return xx(yy, k); }
+                                });
+                            }
+                        })
+                    };
+                })
+            };
+        })
+    };
+}
+
+function eval_jot_cpst(s, k) {
+    if(s.length > 1) {
+        return function () {
+            return eval_jot_cpst(
+                s.substr(0, s.length - 1),
+                function(F) {
+                    if(s[s.length - 1] == '0') {
+                        return function () {
+                            return F(S_cpst, function (FS) { 
+                                return function () { return FS(K_cpst, k); } 
+                            });
+                        }
+                    }
+                    else if(s[s.length - 1] == '1') {
+                        return function () {
+                            return K_cpst(F, function (KF) {
+                                return function () { return S_cpst(KF, k); }
+                            });
+                        }
+                    }
+                    else {
+                        console.error("Error parsing " + s);
+                    }
+                });
+        }
+    }
+    else return function () {
+        return S_cpst(K_cpst, function (SK) {
+            return function () { return SK(K_cpst, k); }
+        });
+    }
+}
+
+function drive_cpst(p, limit) {
+    var count = 0;
+    var done = false;
+    var thunk = eval_jot_cpst(p, function (x) {
+        //alert("done!");
+        return function () { done = true; }
+    });
+    while(count < limit && !done) {
+        //console.info(thunk);
+        thunk = thunk();
+        count++;
+    }
+    return count;
+}
+
+
+// These functions are adapted from http://en.wikipedia.org/wiki/Hilbert_curve
+
+//convert (x,y) to d
+function xy2d(n, x, y) {
+    var rx, ry, s, d=0;
+
+    function rot() {
+        if (ry == 0) {
+            if (rx == 1) {
+                x = n-1 - x;
+                y = n-1 - y;
+            }
+            
+            //Swap x and y
+            var t = x;
+            x = y;
+            y = t;
+        }
+    }
+    
+    for (s=n/2; s>0; s/=2) {
+        rx = (x & s) > 0;
+        ry = (y & s) > 0;
+        d += s * s * ((3 * rx) ^ ry);
+        rot();
+    }
+    return d;
+}
+
+// And here's my own drawing function
+function render(c) {
+    //alert("hi");
+    var ctx = c.getContext("2d");
+    
+    var limit = 200;
+    var N = 512;
+    var w = c.width / N;
+
+    var log2 = Math.log(2);
+
+    for(var i = 0; i < N; i++) {
+        for(var j = 0; j < N; j++) {
+            //console.info("" + i + ", " + j);
+            var p = xy2d(N, i, j);
+            //console.info(p);
+            p /= N * N;
+            p = decode_jot(p);
+            //console.info("Evaluating " + p);
+            var c = drive_cpst(p, limit);
+            //console.info("" + p + " " + c);
+            if(c < limit) {
+                c /= limit;
+                //c = Math.log(c + 1);
+                c = Math.log(c + 1) / log2;
+                c = Math.floor(c * 256);
+                ctx.fillStyle = "rgb(" + c + ", " + c + ", " + c + ")";
+            }
+            else {
+                ctx.fillStyle = "#FF0000";
+            }
+
+            ctx.fillRect(i * w, j * w, (i + 1) * w, (j + 1) * w);
+        }
+    }
 }
