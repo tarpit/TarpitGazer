13b1f35480e55a66e0131e16b0d0e7b4c273dec8
Eric Holk <eric.holk@gmail.com>
2013-02-22 21:16:41 -0500

    Added encoding and decoding functions.

--- tarpit.js
+++ tarpit.js
@@ -47,3 +47,57 @@ function S_cps(x, k) {
     });
 }
 
+//function compile_jot_cps(s) {
+//    if(s.length > 1) {
+//        F = compile_jot_cps(s.substr(0, s.length - 1));
+//
+//        if(s[s.length - 1] == '0') {
+//            return F(S, function (FS) { return Fs(K, k); });
+//        }
+//        else if(s[s.length - 1] == '1') {
+//            return S(K(F));
+//        }
+//        else {
+//            console.error("Error parsing " + s);
+//        }
+//    }
+//    else return S(K)(K); // I
+//}
+
+function encode_jot(s) {
+    var x = 0.5;
+    var y = 0.25;
+    for(var i = 0; i < s.length; i++) {
+        if(s[i] == '0') {
+            x -= y;
+        }
+        else if(s[i] == '1') {
+            x += y;
+        }
+        y /= 2;
+    }
+    return x;
+}
+
+function decode_jot(x) {
+    var s = '';
+    var y = 0.5;
+    var z = 0.25;
+
+    //console.info(x);
+    while(x != y) {
+        //console.info(y);
+        if(x > y) {
+            s += '1';
+            y += z;
+        }
+        else if(x < y) {
+            s += '0';
+            y -= z;
+        }
+        z /= 2;
+    }
+    //console.info(y);
+
+    return s;
+}
